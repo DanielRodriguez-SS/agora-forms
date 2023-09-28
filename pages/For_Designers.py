216d091cc97d7aec1bf7cd69937579dc3819d715
DanielRodriguez-SS <83928934+DanielRodriguez-SS@users.noreply.github.com>
2023-09-28 10:13:37 +0930

    Tools For Designers Page

---
+++ pages/For_Designers.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import frontend.styling as css
+
+st.set_page_config(page_title="Tools|ForDesigners",
+                       page_icon="🧊")
+css.hide_streamlit_defualt_menu_footer()
