7c3591169cc92d17546cd05c5e26925dd6e9d3a7
DanielRodriguez-SS <83928934+DanielRodriguez-SS@users.noreply.github.com>
2023-09-29 15:52:25 +0930

    Finally

--- pages/For_Designers.py
+++ pages/For_Designers.py
@@ -46,6 +46,53 @@ image_data_1200x900 = {
 }
 products_images = []
 
+st.markdown("<h1>Images URLs Builder</h1>",unsafe_allow_html=True)
+with st.expander("Need Help?"):
+    col1, col2 = st.columns([1,1],gap="medium")
+    with col1:
+        st.markdown("""<div style="background-color:rgb(239,48,57);height:1.5rem;width:1.5rem;border-radius:50%;text-align:center;margin-bottom:.5rem;margin-top:.5rem;">1</div>""", unsafe_allow_html=True)
+
+        st.markdown('''
+                    :orange[Product Name:] Type the name of the product for images you want to create links for.
+                    ''')
+        st.divider()
+        st.markdown('''
+                    :orange[Category:] This one needs to match with the category on the :blue[*repo*] dir where you will be uploading images.\n
+                    * For AGORA :blue[repo]: /content/dam/tcom/lego/2022/accessories/products/:red[category]/
+                    * For Non-AGORA :blue[repo]: /content/dam/tcom/devices/general/hardware/:red[category]/
+                    ''')
+        st.divider()
+        st.markdown('''
+                    :orange[Sub-Category:] This should align with a general name for the product excluding its color.\n
+                    * For AGORA :blue[repo]: /content/dam/tcom/lego/2022/accessories/products/category/:red[sub-category]/
+                    * For Non-AGORA :blue[repo]: /content/dam/tcom/devices/general/hardware/category/:red[sub-category]/
+                    ''')
+        st.divider()
+        st.markdown('''
+                    :orange[Color:] This should align with the product's color.\n
+                    * For AGORA :blue[repo]: /content/dam/tcom/lego/2022/accessories/products/category/sub-category/:red[color]/
+                    * For Non-AGORA :blue[repo]: /content/dam/tcom/devices/general/hardware/category/sub-category/:red[color]/
+                    ''')
+        st.divider()
+        st.markdown('''
+                    :orange[Hexcode:] Type the color of product on Hexadicimal and hit ENTER
+                    ''')
+
+    with col2:
+        st.markdown("""<div style="background-color:rgb(239,48,57);height:1.5rem;width:1.5rem;border-radius:50%;text-align:center;margin-bottom:.5rem;margin-top:.5rem;">2</div>""", unsafe_allow_html=True)
+        st.markdown('''
+                    :orange[Select all images:] Once "Browse files" appear, click on it and select all images you have for that product, for AGORA and Non-AGORA :blue[repo] all combined.
+            ''')
+        st.image('select_files.png')
+        st.divider()
+        st.markdown('''
+                    :orange[Save:] Once all files are uploaded clicl on Save and repeat the process from step 1 to add as many products you need.
+                ''')
+        st.divider()
+        st.markdown('''
+                    :orange[Export .xlsx:] When you have saved all products, next click on "Export .xlsx".
+                ''')
+
 col_inputs, col_outputs = st.columns([1,1], gap="medium")
 
 with col_inputs:
@@ -72,19 +119,19 @@ if product_name and category and sub_category and color and hex_code:
                 image_data_800x800['Name'].append(product_name)
                 image_data_800x800['Size'].append('800x800')
                 image_data_800x800['Hex Color'].append(hex_code)
-                image_data_800x800['Url'].append(f'{REPO_1}{category}/{sub_category}/{color}{file.name}')
+                image_data_800x800['Url'].append(f'{REPO_1}{category}/{sub_category}/{color}/{file.name}')
 
             elif 'landscape' not in file.name:
                 image_data_900x1200['Name'].append(product_name)
                 image_data_900x1200['Size'].append('900x1200')
                 image_data_900x1200['Hex Color'].append(hex_code)
-                image_data_900x1200['Url'].append(f'{REPO_1}{category}/{sub_category}/{color}{file.name}')
+                image_data_900x1200['Url'].append(f'{REPO_1}{category}/{sub_category}/{color}/{file.name}')
 
             else:
                 image_data_1200x900['Name'].append(product_name)
                 image_data_1200x900['Size'].append('1200x900')
                 image_data_1200x900['Hex Color'].append(hex_code)
-                image_data_1200x900['Url'].append(f'{REPO_1}{category}/{sub_category}/{color}{file.name}')
+                image_data_1200x900['Url'].append(f'{REPO_1}{category}/{sub_category}/{color}/{file.name}')
 
         temp_image_data = {
             "Name":image_data_800x800['Name']+image_data_900x1200['Name']+image_data_1200x900['Name'],
