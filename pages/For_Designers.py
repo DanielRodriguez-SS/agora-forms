cdb7e37f61450500e815a17facb6600346cdbd17
DanielRodriguez-SS <Daniel.rodriguez.marin86@gmail.com>
2023-10-10 15:54:29 +1030

    Adding SKUs field

--- pages/For_Designers.py
+++ pages/For_Designers.py
@@ -96,6 +96,7 @@ with st.expander("Need Help?"):
 col_inputs, col_outputs = st.columns([1,1], gap="medium")
 
 with col_inputs:
+    product_sku  = st.text_input('Product SKU', placeholder='100252286')
     product_name = st.text_input('Product Name', placeholder='Sprout-AudioPlus TWS Earbuds-Black')
     category = st.text_input('Category', placeholder='earbuds')
     sub_category = st.text_input('Sub-Category', placeholder='ghdwerb-saptw')
@@ -111,29 +112,33 @@ def add_product_images(temp_images_data):
 def delete_product(item):
     st.session_state.images_data.pop(item)
 
-if product_name and category and sub_category and color and hex_code:
+if product_sku and product_name and category and sub_category and color and hex_code:
     files = col_inputs.file_uploader('Slect all images', key=st.session_state.file_uploader_key, accept_multiple_files=True)
     if files:
         for file in files:
             if '800x800' in file.name:
+                image_data_800x800['SKU'].append(product_sku)
                 image_data_800x800['Name'].append(product_name)
                 image_data_800x800['Size'].append('800x800')
                 image_data_800x800['Hex Color'].append(hex_code)
                 image_data_800x800['Url'].append(f'{REPO_1}{category}/{sub_category}/{color}/{file.name}')
 
             elif 'landscape' not in file.name:
+                image_data_800x800['SKU'].append(product_sku)
                 image_data_900x1200['Name'].append(product_name)
                 image_data_900x1200['Size'].append('900x1200')
                 image_data_900x1200['Hex Color'].append(hex_code)
                 image_data_900x1200['Url'].append(f'{REPO_2}{category}/{sub_category}/{color}/{file.name}')
 
             else:
+                image_data_800x800['SKU'].append(product_sku)
                 image_data_1200x900['Name'].append(product_name)
                 image_data_1200x900['Size'].append('1200x900')
                 image_data_1200x900['Hex Color'].append(hex_code)
                 image_data_1200x900['Url'].append(f'{REPO_2}{category}/{sub_category}/{color}/{file.name}')
 
         temp_image_data = {
+            "SKU": image_data_800x800['SKU']+image_data_900x1200['SKU']+image_data_1200x900['SKU'],
             "Name":image_data_800x800['Name']+image_data_900x1200['Name']+image_data_1200x900['Name'],
             "Size":image_data_800x800['Size']+image_data_900x1200['Size']+image_data_1200x900['Size'],
             'Hex Color': image_data_800x800['Hex Color']+image_data_900x1200['Hex Color']+image_data_1200x900['Hex Color'],
