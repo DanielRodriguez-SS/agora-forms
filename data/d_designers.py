22ef3e85810ed7974af18e90b123c51baaeaa98f
DanielRodriguez-SS <Daniel.rodriguez.marin86@gmail.com>
2023-10-13 14:55:53 +1030

    Changes

---
+++ data/d_designers.py
@@ -0,0 +1,11 @@
+############## ACCESSORIES REPOS
+# Only for 800X800 Images
+REPO_1 = "https://www.telstra.com.au/content/dam/tcom/lego/2022/accessories/products/"
+# Other Images Sizes
+REPO_2 = "https://www.telstra.com.au/content/dam/tcom/devices/general/hardware/"
+
+############## MOBILITY REPOS
+# Only for 800X800 Images
+REPO_3 = "https://www.telstra.com.au/content/dam/tcom/library/prd-product/general/"
+# Other Images Sizes
+REPO_4 = "https://www.telstra.com.au/content/dam/tcom/devices/"
--- pages/For_Designers.py
+++ pages/For_Designers.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import frontend.styling as css
 import frontend.help.for_designers_page as help_hint
-import features.designers as feature
+import features.f_designers as feature
+import data.d_designers as data
 import pandas as pd
 
 if 'images_data' not in st.session_state:
@@ -19,40 +20,10 @@ with st.sidebar:
     if st.button('Reset'):
         st.rerun()
 
-# Only for 800X800 Images
-REPO_1 = "https://www.telstra.com.au/content/dam/tcom/lego/2022/accessories/products/"
-# Other Images Sizes
-REPO_2 = "https://www.telstra.com.au/content/dam/tcom/devices/general/hardware/"
-
-image_data_800x800 = {
-    "SKU":[],
-    "Name":[],
-    "Size":[],
-    "Color":[],
-    "Hex Color":[],
-    "Url":[]
-}
-image_data_900x1200 = {
-    "SKU":[],
-    "Name":[],
-    "Size":[],
-    "Color":[],
-    "Hex Color":[],
-    "Url":[]
-}
-image_data_1200x900 = {
-    "SKU":[],
-    "Name":[],
-    "Size":[],
-    "Color":[],
-    "Hex Color":[],
-    "Url":[]
-}
-products_images = []
-
 st.markdown("<h1>🦾 Let's Automate The DAM Thing</h1>",unsafe_allow_html=True)
 st.markdown("<h3>Images URLs Builder</h3>",unsafe_allow_html=True)
 help_hint.display_help()
+segment = st.selectbox('Segment',['Accessories','Mobility'])
 
 col_inputs, col_outputs = st.columns([1,1], gap="medium")
 
@@ -67,37 +38,23 @@ with col_inputs:
 if product_sku and product_name and category and sub_category and color and hex_code:
     files = col_inputs.file_uploader('Slect all images', key=st.session_state.file_uploader_key, accept_multiple_files=True)
     if files:
-        for file in files:
-            if '800x800' in file.name:
-                image_data_800x800['SKU'].append(product_sku)
-                image_data_800x800['Name'].append(product_name)
-                image_data_800x800['Size'].append('800x800')
-                image_data_800x800['Color'].append(color)
-                image_data_800x800['Hex Color'].append(hex_code)
-                image_data_800x800['Url'].append(f'{REPO_1}{category}/{sub_category}/{color}/{file.name}')
-            elif 'landscape' not in file.name:
-                image_data_800x800['SKU'].append(product_sku)
-                image_data_900x1200['Name'].append(product_name)
-                image_data_900x1200['Size'].append('900x1200')
-                image_data_800x800['Color'].append(color)
-                image_data_900x1200['Hex Color'].append(hex_code)
-                image_data_900x1200['Url'].append(f'{REPO_2}{category}/{sub_category}/{color}/{file.name}')
-            else:
-                image_data_800x800['SKU'].append(product_sku)
-                image_data_1200x900['Name'].append(product_name)
-                image_data_1200x900['Size'].append('1200x900')
-                image_data_800x800['Color'].append(color)
-                image_data_1200x900['Hex Color'].append(hex_code)
-                image_data_1200x900['Url'].append(f'{REPO_2}{category}/{sub_category}/{color}/{file.name}')
-        temp_image_data = {
-            "SKU":['']+image_data_800x800['SKU']+image_data_900x1200['SKU']+image_data_1200x900['SKU'],
-            "Name":['']+image_data_800x800['Name']+image_data_900x1200['Name']+image_data_1200x900['Name'],
-            "Size":['']+image_data_800x800['Size']+image_data_900x1200['Size']+image_data_1200x900['Size'],
-            "Color":['']+image_data_800x800['Color']+image_data_900x1200['Color']+image_data_1200x900['Color'],
-            'Hex Color':['']+image_data_800x800['Hex Color']+image_data_900x1200['Hex Color']+image_data_1200x900['Hex Color'],
-            'Url':['']+image_data_800x800['Url']+image_data_900x1200['Url']+image_data_1200x900['Url']
-        }
-        save = st.button('Save', on_click=feature.add_product_images, args=[temp_image_data])
+        if segment == 'Accessories':
+            temp_image_data = feature.process_images_names_for_acc_tplus(files,
+                                                                     product_sku,
+                                                                     product_name,
+                                                                     category,
+                                                                     sub_category,
+                                                                     color,
+                                                                     hex_code)
+        else:
+            temp_image_data = feature.process_images_names_for_mobility(files,
+                                                                     product_sku,
+                                                                     product_name,
+                                                                     category,
+                                                                     sub_category,
+                                                                     color,
+                                                                     hex_code)
+        st.button('Save', on_click=feature.add_product_images, args=[temp_image_data])
 
 with col_outputs:
     number_of_products_saved = len(st.session_state.images_data)
@@ -120,10 +77,9 @@ with col_outputs:
             df_to_export = super_dataFrame   
         else:
             df_to_export = pd.DataFrame.from_dict(st.session_state.images_data[0])
-        excel_file_name = st.text_input('File Name:', placeholder='MyURLs')    
+        excel_file_name = st.text_input('File Name',help='To export file, enter a name.', placeholder='MyURLs')    
         if bool(excel_file_name):
             data_to_export = feature.export_excel(df_to_export)
-            st.button('Test URLs', on_click=feature.open_urls)
             st.download_button(
                 label="Export .xlsx",
                 data=data_to_export,
