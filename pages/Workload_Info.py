c20f206dc7bea74f47a85b7973b268bef87df6e0
SWimplifyDev <83928934+DanielRodriguez-SS@users.noreply.github.com>
2023-12-19 16:11:36 +1030

    Mongo PIP

---
+++ pages/Workload_Info.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import frontend.styling as css
+st.set_page_config(page_title="AgoraOps | Workload",
+                       page_icon="🧊")
+css.hide_streamlit_defualt_menu_footer()
+st.info("Sit tight, this page will be live soon.",icon='😀')
