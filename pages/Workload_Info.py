d23b3b33470eb02a469370f2920e50298734aa21
SWimplifyDev <83928934+DanielRodriguez-SS@users.noreply.github.com>
2023-12-20 17:56:44 +1030

    Jiras_T

--- pages/Workload_Info.py
+++ pages/Workload_Info.py
@@ -2,33 +2,46 @@ import streamlit as st
 import frontend.styling as css
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import database as db
+import pandas as pd
+from datetime import datetime
 
 st.set_page_config(page_title="AgoraOps | Workload",
-                       page_icon="🧊")
-css.hide_streamlit_defualt_menu_footer()
+                       page_icon="🧊",layout="wide")
+# css.hide_streamlit_defualt_menu_footer()
 
 
-st.info("Sit tight, this page will be live soon.",icon='😀')
+client = db.connect_to_db()
+jiras_collection = client['gtm_trading']['jiras']
+df = pd.DataFrame(jiras_collection.find().sort('Due Date', -1))
+df = df.drop(columns=['_id'])
+df['Due Date'] = pd.to_datetime(df['Due Date'])
+# df['Month'] = df['Due Date'].dt.month
+df['Month'] = df['Due Date'].dt.strftime('%B')
+df['Year'] = df['Due Date'].dt.year
+df['Week'] = df['Due Date'].dt.day // 7+1
 
-uri = "mongodb+srv://agoraops:ylLRaVhJuw0LmNNF@cluster0.av42zfn.mongodb.net/?retryWrites=true&w=majority"
+current_date = datetime.now()
+future_dates_df = df[df['Due Date'] >= current_date]
 
-# Create a new client and connect to the server
-client = MongoClient(uri,tls=True,tlsAllowInvalidCertificates=True,server_api=ServerApi('1'))
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-    #Get a database
-    db = client.gtm_trading
-    # List all collections on DB
-    collections = db.list_collection_names()
-    print(collections)
-    collection  = client.gtm_trading.jiras
-    cursor = collection.find()
-    for c in cursor:
-        print(c)
-        st.write(c)
-except Exception as e:
-    print(e)
+grouped = future_dates_df.groupby(['Year','Month','Week'])
 
+# df['Due Date'] = df['Due Date'].apply(lambda x: x.strftime('%Y-%m-%d'))
+# for jira in jiras_collection.find():
+#     st.write(jira)
+st.dataframe(df,hide_index=True,use_container_width=True)
+graph_dict = {}
+# graph_dict['x'] = []
+# graph_dict['y'] = []
+for (year,month,week), group in grouped:
+    st.write(f'wk {week} / {month}, {year}')
+    group = group.drop(columns=['Month', 'Year','Week'])
+    # group['Week Of Month'] = group['Due Date'].dt.day // 7+1
+    st.write(len(group))
+    st.dataframe(group,hide_index=True,use_container_width=True)
+    graph_dict[f'{year} wk {week} / {month}'] = len(group)
+    # graph_dict['x'].append(f'wk {week} / {month}, {year}')
+    # graph_dict['y'].append(len(group))
+st.write(graph_dict)
+st.bar_chart(graph_dict)
