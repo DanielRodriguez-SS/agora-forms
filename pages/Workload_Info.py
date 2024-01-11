9b0b9a7ba919376749c3fc3f8be87e1a145f2e6c
SWimplifyDev <83928934+DanielRodriguez-SS@users.noreply.github.com>
2024-01-11 12:59:03 +1030

    Update WorkUpl

--- pages/Workload_Info.py
+++ pages/Workload_Info.py
@@ -1,47 +1,62 @@
 import streamlit as st
-import frontend.styling as css
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
 import database as db
 import pandas as pd
 from datetime import datetime
-
+import calendar
+#'''
+#Dynamic Tabs Creation Ref:
+#https://docs.kanaries.net/topics/Streamlit/streamlit-tabs
+#'''
 st.set_page_config(page_title="AgoraOps | Workload",
                        page_icon="🧊",layout="wide")
-# css.hide_streamlit_defualt_menu_footer()
 
+def plural_word(len_items:int,word:str) -> str:
+    if len_items > 1:
+        return f'{word}s'
+    else:
+        return word
 
+# Get client from DB connection
 client = db.connect_to_db()
+# Get all documents on JIRAs collection from the gtt_trading database
 jiras_collection = client['gtm_trading']['jiras']
+# Set a dataframe with results from the cursor and sorted in order by Due Date
 df = pd.DataFrame(jiras_collection.find().sort('Due Date', -1))
+# Drop the _id column
 df = df.drop(columns=['_id'])
+# From str to datetime format on "Due Date" Column
 df['Due Date'] = pd.to_datetime(df['Due Date'])
-# df['Month'] = df['Due Date'].dt.month
-df['Month'] = df['Due Date'].dt.strftime('%B')
+# Add 3 columns to df from "Due Date" get the Month, Year and Week number of the month
+df['Month'] = df['Due Date'].dt.month
 df['Year'] = df['Due Date'].dt.year
 df['Week'] = df['Due Date'].dt.day // 7+1
-
+# Save the current date
 current_date = datetime.now()
-future_dates_df = df[df['Due Date'] >= current_date]
-
-
+# Get a new dataframe with only Jiras which Due Date is in future and sort them ascending
+future_dates_df = df[df['Due Date'] >= current_date].sort_values(by='Due Date', ascending=True)
+# Gruped the new filtered dataframe by 'Year' 'Month' and 'Week'
 grouped = future_dates_df.groupby(['Year','Month','Week'])
+future_dates_df = future_dates_df.drop(columns=['Month', 'Year','Week'])
 
-# df['Due Date'] = df['Due Date'].apply(lambda x: x.strftime('%Y-%m-%d'))
-# for jira in jiras_collection.find():
-#     st.write(jira)
-st.dataframe(df,hide_index=True,use_container_width=True)
 graph_dict = {}
-# graph_dict['x'] = []
-# graph_dict['y'] = []
+grouped_dfs = {}
+
 for (year,month,week), group in grouped:
-    st.write(f'wk {week} / {month}, {year}')
     group = group.drop(columns=['Month', 'Year','Week'])
-    # group['Week Of Month'] = group['Due Date'].dt.day // 7+1
-    st.write(len(group))
-    st.dataframe(group,hide_index=True,use_container_width=True)
-    graph_dict[f'{year} wk {week} / {month}'] = len(group)
-    # graph_dict['x'].append(f'wk {week} / {month}, {year}')
-    # graph_dict['y'].append(len(group))
-st.write(graph_dict)
+    grouped_dfs[f'{calendar.month_abbr[month]} {year} wk{week}'] = group
+    graph_dict[f'{calendar.month_abbr[month]} {year} - wk {week}'] = len(group)
+
+st.markdown(f'# Task Pipeline Forecast')
+st.markdown(f'{len(future_dates_df)} {plural_word(len(future_dates_df),'Ticket')}')
 st.bar_chart(graph_dict)
+st.markdown('# Weekly Task Breakdown')
+tab_list = list(grouped_dfs)
+tabs = st.tabs(tab_list)
+
+for df,tab in zip(grouped_dfs, tabs):
+    with tab:
+        st.write(f'{len(grouped_dfs[df])} {plural_word(len(grouped_dfs[df]),'Ticket')}')
+        st.dataframe(grouped_dfs[df],hide_index=True,use_container_width=True)
+
+st.markdown(f'# Work Queue Catalog')
+st.dataframe(future_dates_df, hide_index=True, use_container_width=True)
