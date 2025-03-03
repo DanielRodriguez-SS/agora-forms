a4e1b7d701db25ec7184ef7ff0585a29e688bcbe
SWimplifyDev <Daniel.rodriguez.marin86@gmail.com>
2025-03-03 12:23:23 +1030

    More secure

--- database.py
+++ database.py
@@ -8,7 +8,6 @@ https://pymongo.readthedocs.io/en/stable/tutorial.html
 
 def connect_to_db()->MongoClient:
     uri = f"mongodb+srv://{st.secrets["database"]["user"]}:{st.secrets["database"]["password"]}@cluster0.av42zfn.mongodb.net/?retryWrites=true&w=majority"
-    #uri = "mongodb+srv://agoraops:ylLRaVhJuw0LmNNF@cluster0.av42zfn.mongodb.net/?retryWrites=true&w=majority"
     # Create a new client and connect to the server
     client = MongoClient(uri,tls=True,tlsAllowInvalidCertificates=True,server_api=ServerApi('1'))
     # Send a ping to confirm a successful connection
